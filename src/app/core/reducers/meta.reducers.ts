a237aab8c916b6b3bdf0438de88da76a2fbd7462
Michael Lage <gs.urmomma@gmail.com>
2019-05-29 16:41:05 -0600

    Working
    - CoreModule for main angular stuff, ngrx store
    - Setup main dependencies with a blank root state to be lazily loaded, or not, depending on the apps need

---
+++ src/app/core/reducers/meta.reducers.ts
@@ -0,0 +1,22 @@
+import { ActionReducer, MetaReducer } from '@ngrx/store'
+
+import { RootState } from './root.state'
+import { environment } from '../../../environments/environment'
+
+export function logger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function(state, action) {
+    const currentState = state
+    const nextState = reducer(currentState, action)
+
+    console.group(action.type)
+    console.log(`%c prev state`, `color: #9E9E9E; font-weight: bold`, state)
+    console.log(`%c action`, `color: #03A9F4; font-weight: bold`, action)
+    console.log(`%c next state`, `color: #4CAF50; font-weight: bold`, nextState)
+    console.groupEnd()
+
+    return nextState
+  }
+}
+
+export const metaReducers: MetaReducer<RootState>[] =
+  environment.production ? [] : [logger];
