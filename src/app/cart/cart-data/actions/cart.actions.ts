179860fa71c97197ae5cd34b6f77b68adf5511f4
Michael Lage <gs.urmomma@gmail.com>
2019-05-31 22:02:26 -0600

    Tweak
    - Added a error message when trying to add a book to the basket more than once

--- src/app/cart/cart-data/actions/cart.actions.ts
+++ src/app/cart/cart-data/actions/cart.actions.ts
@@ -3,10 +3,13 @@ import { Action } from '@ngrx/store'
 export enum CartActionTypes {
   AttemptToAddBookToCart = '[Cart] Attempt to Add Book To Cart',
   AddBookToCart = '[Cart] Added Book to Cart',
+  AlreadyHaveBookInCartError = '[Cart] Attempt to Add Book Error: Already In Cart',
   RemoveBookFromCart = '[Cart] Removed Book from Cart',
   ClearCart = '[Cart] Removed all Books from Cart',
 }
 
+// @todo What should the pattern in naming actions be?
+// We could follow the BooksActions of Request*, Request*Success, & Request*Error for consistency?
 export class AttemptToAddBookToCart implements Action {
   readonly type = CartActionTypes.AttemptToAddBookToCart
 
@@ -17,6 +20,10 @@ export class AddBookToCart implements Action {
 
   constructor(public payload: {bookId: number}) {}
 }
+export class AlreadyHaveBookInCartError implements Action {
+  readonly type = CartActionTypes.AlreadyHaveBookInCartError
+}
+// The above naming pattern?
 export class RemoveBookFromCart implements Action {
   readonly type = CartActionTypes.RemoveBookFromCart
 
--- src/app/cart/cart-data/effects/cart.effects.ts
+++ src/app/cart/cart-data/effects/cart.effects.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core'
+import { MatSnackBar } from '@angular/material'
 
 import { Observable } from 'rxjs'
-import { withLatestFrom, map } from 'rxjs/operators'
+import { withLatestFrom, map, tap } from 'rxjs/operators'
 import { Actions, Effect, ofType } from '@ngrx/effects'
 import { Action, select, Store } from '@ngrx/store'
 
-import { AddBookToCart, AttemptToAddBookToCart, CartActionTypes } from '../actions/cart.actions'
+import { AddBookToCart, AttemptToAddBookToCart, CartActionTypes, AlreadyHaveBookInCartError } from '../actions/cart.actions'
 import { selectCartStatusBookIds } from '../selectors/cart-status.selectors'
+
 
 @Injectable()
 export class CartEffects {
   constructor(
     private actions$: Actions,
     private store: Store<any>,
+    private snackbar: MatSnackBar
   ) {}
 
-  @Effect({
-    dispatch: false
-  })
+  @Effect()
   addBookToCart$: Observable<Action> = this.actions$
     .pipe(
       ofType<AttemptToAddBookToCart>(CartActionTypes.AttemptToAddBookToCart),
@@ -29,10 +30,21 @@ export class CartEffects {
         // Our business logic: Users can only add 1 copy of each Book to their Cart
         // It's a library, not a Store ;)
         if (!bookId) {
-          this.store.dispatch(new AddBookToCart({bookId: action.payload.bookId}))
+          return new AddBookToCart({bookId: action.payload.bookId})
+        } else {
+          return new AlreadyHaveBookInCartError()
         }
-
-        return action
       })
+    )
+
+  @Effect({
+    dispatch: false
+  })
+  showErrorNotificationBookAlreadyInCart$: Observable<Action> = this.actions$
+    .pipe(
+      ofType<AlreadyHaveBookInCartError>(CartActionTypes.AlreadyHaveBookInCartError),
+      tap(() => this.snackbar.open(`That book is already in your basket`, 'Close', {
+        duration: 3000
+      }))
     )
 }
