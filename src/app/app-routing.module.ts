079ef1df61236904ce6038d2200456b60d8d8c3a
Michael Lage <gs.urmomma@gmail.com>
2019-06-01 00:03:42 -0600

    Tweak
    - v8 ng syntax for children lazily loaded from module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ export const RootRoutes: Routes = [
     children: [      
       {
         path: '',
-        loadChildren: './books/books-views/books-views.module#BooksViewsModule'
+        loadChildren: () => import('./books/books-views/books-views.module').then(m => m.BooksViewsModule)
       }
     ]
   }
