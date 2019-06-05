bd35570a458fe4455950392ab7031ec654ecda1c
Michael Lage <gs.urmomma@gmail.com>
2019-06-05 13:32:39 -0600

    Notes
    - expanding why we de-coupled the form component submit button from the actual form, since it would have taken less code to include it in the dynamic-form-live component. Sometimes design ui dictates code requirements and we needed to be able to place that submit button anywhere

--- src/app/checkout/checkout-widgets/checkout-submit-button/smart/checkout-submit-button.component.ts
+++ src/app/checkout/checkout-widgets/checkout-submit-button/smart/checkout-submit-button.component.ts
@@ -8,6 +8,11 @@ import { selectCartStatusBooksCount } from 'src/app/cart/cart-data/selectors/car
 import { selectCheckoutRequestUser } from 'src/app/checkout/checkout-data/selectors/checkout-request.selectors'
 import { CheckoutSubmit } from 'src/app/checkout/checkout-data/actions/checkout.actions';
 
+/**
+ * @description   A lot of work went into de-coupling this main form Submit button
+ *                It was done to get flexibility in where we lay the button in the DOM
+ *                Ideally, we would have instead used a design that coupled the submit button with the form, to decrease overall code size, but sometimes UI design dictates code
+ */
 @Component({
   selector: 'checkout-submit-button',
   template: `
@@ -27,25 +32,34 @@ export class CheckoutSubmitButtonComponent {
       store.pipe(select(selectCheckoutRequestUser))
     ).pipe(
       map(([cartBooksCount, checkoutUser]) => {
+        // books min count check
         if (cartBooksCount === 0) {
           return true
         }
 
+        // null check
         if (checkoutUser === null) {
           return true
         }
-
         if (checkoutUser.first_name === null ||
             checkoutUser.last_name === null ||
             checkoutUser.library_card_id === null) {
           return true
         }
 
+        // min lengths check
         if (checkoutUser.first_name === '' ||
             checkoutUser.last_name === '') {
           return true
         }
 
+        // Max length checks
+        if (checkoutUser.first_name.length > 255 ||
+            checkoutUser.last_name.length > 255) {
+          return true
+        }
+
+        // library card ID length check
         if (checkoutUser.library_card_id.length !== 8) {
           return true
         }
