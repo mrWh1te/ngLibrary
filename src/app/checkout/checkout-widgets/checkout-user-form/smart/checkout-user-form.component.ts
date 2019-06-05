11d332a1fdc5cd85918b7e46f174a2e4f131045c
Michael Lage <gs.urmomma@gmail.com>
2019-06-05 08:37:58 -0600

    Working
    - Set the default values for CheckoutUserForm from store, only on init of the component

--- src/app/checkout/checkout-widgets/checkout-user-form/smart/checkout-user-form.component.ts
+++ src/app/checkout/checkout-widgets/checkout-user-form/smart/checkout-user-form.component.ts
@@ -1,44 +1,56 @@
 import { Component } from "@angular/core"
 
-import { Store } from '@ngrx/store'
+import { Store, select } from '@ngrx/store'
 
 import { DynamicFormConfig } from 'src/app/shared/dynamic-form/models/dynamic-form-config.model'
 import { CheckoutUpdateUserInfo } from 'src/app/checkout/checkout-data/actions/checkout.actions';
 import { User } from 'src/app/checkout/checkout-data/models/user.model'
+import { Observable } from 'rxjs';
+import { selectCheckoutRequestUser } from 'src/app/checkout/checkout-data/selectors/checkout-request.selectors';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'checkout-user-form',
   template: `
     <dynamic-form-live
-      [config]="dynamicFormConfig"
+      [config]="dynamicFormConfig$ | async"
       (onFormChange)="formValueChanged($event)"></dynamic-form-live>
   `
 })
 export class CheckoutUserFormComponent {
-  dynamicFormConfig: DynamicFormConfig = {
-    inputs: [
-      {
-        type: 'text',
-        name: 'first_name',
-        placeholder: 'First name'
-      },
-      {
-        type: 'text',
-        name: 'last_name',
-        placeholder: 'Last name'
-      },
-      {
-        type: 'text',
-        name: 'library_card_id',
-        placeholder: 'Library Card ID',
-        hint: 'Found on the backside of your Library Card'
-      }
-    ]
-  }
+  dynamicFormConfig$: Observable<DynamicFormConfig>
 
   constructor(
     private store: Store<any>
-  ) {}
+  ) {
+    this.dynamicFormConfig$ = store.pipe(
+      select(selectCheckoutRequestUser),
+      take(1), // only do this on initial load, and not on changes to the User data, otherwise we'll be effectively trying to rebuild the form on each change. Currently, the form component doesn't properly watch the config input for changes to then update the form controller's group's control's values or recreate the whole form all together... a lot to be added to DynamicFormModule, come time
+      map(checkoutUser => ({
+        inputs: [
+          {
+            type: 'text',
+            name: 'first_name',
+            placeholder: 'First name',
+            defaultValue: checkoutUser !== null ? checkoutUser.first_name : null
+          },
+          {
+            type: 'text',
+            name: 'last_name',
+            placeholder: 'Last name',
+            defaultValue: checkoutUser !== null ? checkoutUser.last_name : null
+          },
+          {
+            type: 'text',
+            name: 'library_card_id',
+            placeholder: 'Library Card ID',
+            defaultValue: checkoutUser !== null ? checkoutUser.library_card_id : null,
+            hint: 'Found on the backside of your Library Card'
+          }
+        ]
+      }))
+    )
+  }
 
   formValueChanged(user: Partial<User>) {
     // console.log('[CheckoutUserFormComponent] formValueChanged() user = ', user)
