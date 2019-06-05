b9c958a1a67b5a9d5dac8f1a3786fc29c6c2fb69
Michael Lage <gs.urmomma@gmail.com>
2019-06-05 11:39:50 -0600

    Tweak
    - prettified the modal with $primary based colored background for close X icon

--- src/app/shared/modal/modal.module.ts
+++ src/app/shared/modal/modal.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core"
 import { CommonModule } from '@angular/common'
 import { MatIconModule } from '@angular/material'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 import { ModalComponent } from './modal/modal.component'
 
 @NgModule({
   imports: [
     CommonModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule
   ],
   declarations: [
     ModalComponent
