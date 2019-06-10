b3b1bc5128e84fea064707630870ac20a43408c8
Michael Lage <gs.urmomma@gmail.com>
2019-06-10 11:25:31 -0600

    WIP
    - tweak/fix to travis-ci config

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,7 +2,7 @@ describe('Home page', function() {
 
   it('Should contain the word "Inventory"', function() {
     cy.visit('http://localhost:4200')
-    cy.contains("Inventory")
+    cy.contains('Inventory')
   })
   
 })
