75f8295e9c21a33304c9bf458899b8e913b52d40
Michael Lage <gs.urmomma@gmail.com>
2019-06-25 09:47:26 -0600

    WIP
    - basic integration test for OpenLibrary.org API but OpenLibrary.org is down right now! The demo site doesnt work unless you've already visited it recently (so it cached the response data in local storage)
    - more reason to add a backend to this project...

---
+++ cypress/integration/open-library-api.spec.ts
@@ -0,0 +1,20 @@
+import { bookISBNs } from "../../src/app/books/books-data/book-isbns.seed"
+
+describe('Open Library API', function() {
+
+  const firstSeedISBN = bookISBNs[0]
+
+  it('returns JSON', () => {
+    cy.request(`https://openlibrary.org/api/books?bibkeys=ISBN:${firstSeedISBN}&format=json&jscmd=data`)
+      .its('headers')
+      .its('content-type')
+      .should('include', 'application/json')
+  })
+
+  it('Should have a response whose body is a hash map of the first ISBN in our Seed to its actual Book data', () => {
+    // first ISBN, 0451526538, belongs to The Adventures of Tom Sawyer
+    const request = cy.request(`https://openlibrary.org/api/books?bibkeys=ISBN:${firstSeedISBN}&format=json&jscmd=data`)
+    
+    request.its('body').should('include', '<h1>Admin</h1>')
+  })
+})
