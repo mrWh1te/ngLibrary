27b5ebf05f412d1cff1951334ceff2fb74490106
Michael Lage <gs.urmomma@gmail.com>
2019-06-29 00:51:33 -0600

    Clean Up
    - added node-fetch for easy async await try catch block

--- cypress/support/scripts/simplify-open-library-books-json.ts
+++ cypress/support/scripts/simplify-open-library-books-json.ts
@@ -1,33 +1,30 @@
-const https = require('https')
 const bookISBNs = require("../../../src/app/books/books-data/book-isbns.seed.json").bookISBNs
 
 run()
 
-function run() {
-  // grab the copy of the verbose Open Library API Books Response
-  https.get(
-    `https://openlibrary.org/api/books?bibkeys=ISBN:${bookISBNs.join()}&format=json&jscmd=data`, 
-    resp => {
-      let data = ''
-      resp.on('data', (chunk) => {
-        data += chunk
-      })
-  
-      // The whole response has been received. Normalize the result and save
-      resp.on('end', () => {
-        buildCleanJSONFile(JSON.parse(data))
-      })
-    })
-    .on("error", (err) => {
-      console.log(" ❌ Unable to Complete the API Call to OpenLibrary.org: " + err.message)
-    })
+/**
+ * 
+ */
+async function run() {
+  const fetch = require('node-fetch')
+  let booksResponse = JSON.parse('{}')
+
+  try {
+    // grab the copy of the verbose Open Library API Books Response
+    const response = await fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${bookISBNs.join()}&format=json&jscmd=data`)
+    booksResponse = await response.json()
+    
+    writeCleanOLBooksJSONFile(booksResponse)
+  } catch(err) {
+    console.log(" ❌ Unable to Complete the API Call to OpenLibrary.org: " + err.message)
+  }
 }
 
 /**
  * 
  * @param data 
  */
-function buildCleanJSONFile(booksHashMap: JSON): void {
+function writeCleanOLBooksJSONFile(booksHashMap: JSON): void {
   // const booksHashMap = JSON.parse(data)
   const booksHashMapKeys = Object.keys(booksHashMap)
 
@@ -58,7 +55,7 @@ function buildCleanJSONFile(booksHashMap: JSON): void {
       return console.log(" ❌ Unable to write the fixtures/open-library-books-clean.json file: " + err.message)
     }
 
-    console.log(" ✔️  fixtures/open-library-books-clean.json was written & saved");
+    console.log(" ✔️  fixtures/open-library-books-clean.json was written & saved")
   })
 }
 
