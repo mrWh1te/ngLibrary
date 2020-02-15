c251ecd35f67d87d63a9c65cd889f1bb2ca91970
Michael Lage <gs.urmomma@gmail.com>
2020-02-14 20:49:42 -0700

    Added stories for SelectedBookUiComponent
    - added smoother fallback to selected book author as img alt value, so author name is displayed, in addition to book title, when book cover fails to display

---
+++ src/app/book/book-widgets/widgets/selected-book/ui/selected-book-ui.component.stories.ts
@@ -0,0 +1,64 @@
+// Storybook dependencies
+import { moduleMetadata, storiesOf } from '@storybook/angular'
+import { object, withKnobs } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
+
+// Component Storybooking
+import { SelectedBookUiComponent } from './selected-book-ui.component'
+
+// Component Dependency
+import { FlexLayoutModule } from '@angular/flex-layout'
+
+// Actions
+const onAddToBasket = action('Book Added to Basket')
+
+// Template
+const template = `<selected-book-ui [selectedBook]="book" (onAddToBasket)="onAddToBasket()" style="max-width: 250px;margin: 0 auto;display:block;"></selected-book-ui>`
+
+// Stories
+storiesOf('Book/SelectedBookUiComponent', module)
+  .addDecorator(
+    moduleMetadata({
+      declarations: [SelectedBookUiComponent],
+      imports: [FlexLayoutModule]
+    })
+  )
+  .addDecorator(withKnobs)
+  .add('Broken Image Example', () => {
+    const book = object('Book', {
+      id: 5,
+      isbn: 'isbn-example',
+      title: 'Book Title',
+      author: 'Book Author',
+      cover: {
+        large: ''
+      }
+    })
+
+    return {
+      template,
+      props: {
+        book,
+        onAddToBasket
+      }
+    }
+  })
+  .add('Cat Image Example', () => {
+    const book = object('Book', {
+      id: 5,
+      isbn: 'isbn-example',
+      title: 'Book Title',
+      author: 'Book Author',
+      cover: {
+        large: 'https://placekitten.com/250/400'
+      }
+    })
+
+    return {
+      template,
+      props: {
+        book,
+        onAddToBasket
+      }
+    }
+  });
