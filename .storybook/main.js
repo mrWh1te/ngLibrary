2e4acd51017bcc57f2a5478ce2e02ccb3779bf4e
Michael Lage <gs.urmomma@gmail.com>
2020-02-14 20:40:06 -0700

    Added knobs
    - working example, can change the image url

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.ts'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links',
+    '@storybook/addon-notes'
+  ]
 };
--- src/app/book/book-widgets/widgets/book/ui/book-ui.component.stories.ts
+++ src/app/book/book-widgets/widgets/book/ui/book-ui.component.stories.ts
@@ -1,13 +1,21 @@
+// Storybook dependencies
 import { moduleMetadata, storiesOf } from '@storybook/angular'
+import { object, withKnobs } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 
-import { MatCardModule } from '@angular/material/card'
-
+// Component Storybooking
 import { BookUiComponent } from './book-ui.component'
+
+// Component Dependency
+import { MatCardModule } from '@angular/material/card'
 
 // Actions
 const bookClicked = action('Book Clicked')
 
+// Template
+const template = `<book-ui [book]="book" (onSelect)="bookClicked()" style="max-width: 175px;margin: 0 auto;display:block;"></book-ui>`
+
+// Stories
 storiesOf('Book/BookUiComponent', module)
   .addDecorator(
     moduleMetadata({
@@ -15,36 +23,45 @@ storiesOf('Book/BookUiComponent', module)
       imports: [MatCardModule]
     })
   )
-  .add('Broken Image Example', () => ({
-    template: `<book-ui [book]="book" (onSelect)="bookClicked()" style="max-width: 175px;margin: 0 auto;display:block;"></book-ui>`,
-    props: {
-      book: {
-        id: 5,
-        isbn: 'isbn-example',
-        title: 'title',
-        author: 'author',
-        genre: 'genre',
-        summary: 'example summary',
-        description: 'example description',
-      },
-      bookClicked
-    },
-  }))
-  .add('Cat Image Example', () => ({
-    template: `<book-ui [book]="book" (onSelect)="bookClicked()" style="max-width: 175px;margin: 0 auto;display:block;"></book-ui>`,
-    props: {
-      book: {
-        id: 5,
-        isbn: 'isbn-example',
-        title: 'title',
-        author: 'author',
-        genre: 'genre',
-        summary: 'example summary',
-        description: 'example description',
-        cover: {
-          medium: 'https://placekitten.com/200/286'
-        }
-      },
-      bookClicked
-    },
-  }));
+  .addDecorator(withKnobs)
+  .add('Broken Image Example', () => {
+    const book = object('Book', {
+      id: 5,
+      isbn: 'isbn-example',
+      title: 'title',
+      author: 'author',
+      genre: 'genre',
+      summary: 'example summary',
+      description: 'example description',
+    })
+
+    return {
+      template,
+      props: {
+        book,
+        bookClicked
+      }
+    }
+  })
+  .add('Cat Image Example', () => {
+    const book = object('Book', {
+      id: 5,
+      isbn: 'isbn-example',
+      title: 'title',
+      author: 'author',
+      genre: 'genre',
+      summary: 'example summary',
+      description: 'example description',
+      cover: {
+        medium: 'https://placekitten.com/200/286'
+      }
+    })
+
+    return {
+      template,
+      props: {
+        book,
+        bookClicked
+      }
+    }
+  });
